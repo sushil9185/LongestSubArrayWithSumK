ca7727b6d9c5db4db34e6812fcf83dfa50141400
Sushilkumar Pandey <s.pandey.ctr@ad.dot.gov>
2024-08-28 21:56:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace LongestSubArrayWithSumK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3, 1, 1, 1, 1, 4, 2, 3 };
+            Console.WriteLine("Orignal Array");
+            PrintArray(myArray);
+            int k = 3;
+            Console.WriteLine("Longest Sub Array with Sum " + k);
+            Console.WriteLine(LongestSubArray1(myArray,k));
+
+        }
+
+        //optimized
+        static int LongestSubArray2(int[] a, int k)
+        {
+
+
+            return k;
+        }
+
+        //Brute1
+        static int LongestSubArray(int[] a, int k)
+        {
+            int maxlen = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                for(int j = i; j < a.Length; j++)
+                {
+                    int sum = 0;
+                    for (int l = i; l <= j; l++)
+                        sum += a[l];
+                    
+                    if(sum == k)
+                        maxlen = Math.Max(j-i+1, maxlen);
+                }
+            }
+            return maxlen;
+
+        }
+
+        //brute 2
+        static int LongestSubArray1(int[] a, int k)
+        {
+            int maxlen = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < a.Length; j++)
+                {
+                    sum += a[j];
+
+                    if (sum == k)
+                        maxlen = Math.Max(j - i + 1, maxlen);
+                }
+            }
+            return maxlen;
+
+        }
+
+        static void PrintArray(int[] a)
+        {
+            foreach (int i in a)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
